0acbc2118aba1f8a4d62e557a45393c009cc622a
Daniela Gallegos Dupuis <danigallegdup@gmail.com>
2025-02-09 15:05:12 -0800

    Update Mongo_connect.js

--- backend/Mongo_connect.js
+++ backend/Mongo_connect.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://danigallegdup:VkOmd60a03HNRSAL@cluster0.4r5qq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
