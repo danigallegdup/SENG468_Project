db4d828cada1aa98d0667f8542d5d8315999a948
tar3kAlakkad1 <tarek.alakkad03@hotmail.com>
2025-02-16 16:59:48 -0800

    removed unnecessary comments

--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -4,13 +4,10 @@ const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const walletController = require('../controllers/walletController');
 
-// Existing route for wallet transactions
 router.get('/getWalletTransactions', authMiddleware, walletController.getWalletTransactions);
 
-// New route: Retrieve Wallet Balance
 router.get('/getWalletBalance', authMiddleware, walletController.getWalletBalance);
 
-// New route: Add Money to Wallet
 router.post('/addMoneyToWallet', authMiddleware, walletController.addMoneyToWallet);
 
 module.exports = router;
