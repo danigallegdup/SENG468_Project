918acde8d78c94cb98de71d6946d7b39ecf68160
hieuvuong2310 <hieuvuong2310@gmail.com>
2025-02-19 15:59:34 -0800

    Update schema for orders and getStockTransaction

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,16 +1,19 @@
 const { ObjectId } = require("mongodb");
+const mongoose = require("mongoose");
 
 const orderSchema = {
-    _id: ObjectId,          // Unique ID for each order
     user_id: ObjectId,      // ID of the user placing the order
     stock_id: String,       // Stock symbol
     is_buy: Boolean,        // Specifies if order is buy (True) or sell (False)
     order_type: String,     // "MARKET" or "LIMIT"
     quantity: Number,       // Quantity  of stocks in the order
-    price: Number,          // Price per stock (for limit orders)
-    status: String,         // Order status: "IN_PROGRESS", "PARTIALLY_COMPLETE", "COMPLETED"
-    created_at: Date        // Timestamp of order creation
+    stock_price: Number,          // Price per stock (for limit orders)
+    order_status: String,         // Order status: "IN_PROGRESS", "PARTIALLY_COMPLETE", "COMPLETED"
+    created_at: Date,        // Timestamp of order creation
+    stock_tx_id: String,
+    parent_stock_tx_id: String,
+    wallet_tx_id: String
 };
 
-module.exports = orderSchema;       // Export schema for use in MongoDB operations
+module.exports = mongoose.model('Order', orderSchema);     // Export schema for use in MongoDB operations
 
