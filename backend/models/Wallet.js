a9c05f3783da05011e50b038601bd78b97baa78e
hieuvuong2310 <hieuvuong2310@gmail.com>
2025-02-20 03:40:04 -0800

    Add wallet schema

---
+++ backend/models/Wallet.js
@@ -0,0 +1,10 @@
+// models/WalletTransaction.js
+const mongoose = require('mongoose');
+
+const WalletSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  timeStamp: { type: Date, default: Date.now },
+  balance: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Wallet', WalletSchema);
