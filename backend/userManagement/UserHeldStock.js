1f090699a483d8cfb5ac2c76dd78f7119c0f7239
hieuvuong2310 <hieuvuong2310@gmail.com>
2025-03-03 21:38:51 -0800

    Add test files

--- backend/userManagement/UserHeldStock.js
+++ backend/userManagement/UserHeldStock.js
@@ -7,7 +7,7 @@ const UserHeldStockSchema = new mongoose.Schema({
     stock_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Stock', required: true },
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     stock_name: { type: mongoose.Schema.Types.String, required: true },
-    quantity_owned: { type: mongoose.Schema.Types.Number, required: true },
+    quantity_owned: { type: Number, required: true },
     updated_at: { type: mongoose.Schema.Types.Date, required: false }
 
 });
