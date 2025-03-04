7ce59c882ea147e6f4e6a2cfbfa49fa3a999947e
Gabriel Taves <gabriel.taves01@gmail.com>
2025-03-03 17:45:18 -0800

    Small debugging changes

--- backend/order/redis.js
+++ backend/order/redis.js
@@ -5,12 +5,15 @@ redis.js: Responsible for connecting to Redis cache client.
 
 const redis = require("redis");
 
-const REDIS_HOST = process.env.REDIS_HOST || "redis";
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_HOST = "redis";
+const REDIS_PORT = 6379;
 
 // Redis connection
 const redisClient = redis.createClient({
   url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+  socket: {
+    reconnectStrategy: () => 1000, // Reconnect every second
+  }
 });
 
 redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
